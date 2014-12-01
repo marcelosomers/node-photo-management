eec5ad699ab3c7b6c48a5e645fbd285c348179c1
Marcelo Somers <msomers@gmail.com>
2014-11-30 23:01:46 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-node-photo-management
+Photo Management in NodeJS
 =====================
+*NOTE: This script is still very much in beta. Use at your own risk. Any time you are scripting moving files around your file system, you run the risk of data loss. Back up your files first.*
 
-Photo Management Scripts in NodeJS
+Preliminary script for managing my photos using NodeJS. This script reads a source directory and moves photos and videos into directories by year/month. I built this for automatically moving files on my Synology from my automated Dropbox Camera Uploads folder into an organized file structure.
+
+By default, files will live in `photos/yyyy/mm - Month/file.jpg` or for example, `photos/2014/01-January/2014-08-09 17.24.25-2.jpg` (or the videos directory).
+
+## Getting Started
+1. Make sure you have [NodeJS](http://nodejs.org/) installed on your intended machine.
+2. Download `organize.js`
+3. In the directory you plan to run `organize.js` run `npm install mv`
+
+## Configuration
+You'll need to set up several variables for this to run properly:
+
+* `sourceDirectory` - this is the directory the script should analyze for photos and videos (requires a trailing /)
+* `photosDestination` and `videosDestination` - base directory where photos and videos should live (requires a trailing /)
+
+Optionally, you can configure `photosExtensions` and `videoExtensions` if you'd like to add other files types extensions.
+
+## Running
+Once everything is set up, running the script is simply a matter of running a `node organize.js` command.
---
+++ organize.js
@@ -0,0 +1,169 @@
+/**
+ * Synology/Node Photo Organization Script
+ */
+
+
+/**
+ * Source Directory Configuration
+ * This is the source directory where our photo and video files live
+ * IMPORTANT: Make sure your path ends with a /
+ */
+var sourceDirectory = "./";
+
+
+
+/**
+ * Destination Directory Configuration
+ * These are the destination directories where photos and videos should ultimately be stored
+ * Note that within these directories will live year/month (e.g., 2014/10 - October) directories,
+ * so this should be the master directories for photos and videos.
+ * IMPORTANT: Make sure your path ends with a /
+ */
+var photosDestination = "./";
+var videosDestination = "./";
+
+
+
+/**
+ * File Extension Configuration
+ * photoExtensions and videoExtensions are file extensions that will land in
+ * photosDestination and videosDestination as defined above
+ */
+var photoExtensions = [
+    ".jpg",
+    ".gif",
+    ".png"
+]
+
+var videoExtensions = [
+    ".mov",
+    ".mp4",
+    ".3gp"
+]
+
+
+/**
+ * ------------ DON'T EDIT BELOW THIS UNLESS YOU KNOW WHAT YOU'RE DOING ------------
+ */
+
+var fs = require("fs")
+    mv = require("mv");
+
+// Define our months to generate folder names
+var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+// Read the directory defined above and save list of files to an array, files
+fs.readdir(sourceDirectory, function(err, files) {
+    if (err) {
+        throw err;
+    }
+
+    var fileType = null;
+
+    // Filter for only our image files
+    var images = [];
+    var videos = [];
+
+    // Loop over all files in sourceDirectory and determine if they are an image or video
+    for (var i=0, len = files.length; i < len; i++) {
+        var file = files[i];
+        var fileExtension = file.substring(file.length - 4);
+
+        // Add any image file with an extension in photoExtensions to our images object
+        if (photoExtensions.indexOf(fileExtension) > -1) {
+            images.push(file);
+        }
+
+        // Add any video file with an extension in videoExtensions to our videos object
+        if (videoExtensions.indexOf(fileExtension) > -1) {
+            videos.push(file);
+        }
+    }
+
+    // Loop over each image file
+    // Get date metadata from the file name
+    // Move the file
+    for (var i=0, len = images.length; i < len; i++) {
+        var file = images[i];
+
+        // Get the year and month from the file name
+        var fileDate = getDateInfo(file);
+
+        // Define paths
+        var existingFilePath = sourceDirectory + "/" + file;
+        var newFilePath = photosDestination + fileDate.year + "/" + fileDate.monthNumber + " - " + fileDate.monthName + "/" + file;
+
+        console.log(newFilePath);
+
+        // Move the file
+        // mkdirp: Make the required directories if they don't exist
+        // clobber: Don't overwrite any files
+        mv(existingFilePath, newFilePath, {mkdirp: true, clobber: false}, function(err) {
+            if(err) {
+                throw err;
+            }
+
+            console.log("Moving " + file + " to " + newFilePath);
+        });
+    }
+
+    // Loop over each video file
+    // Get date metadata from the file name
+    // Move the file
+    for (var i=0, len = videos.length; i < len; i++) {
+        var file = videos[i];
+
+        // Get the year and month from the file name
+        var fileDate = getDateInfo(file);
+
+        // Define paths
+        var existingFilePath = sourceDirectory + "/" + file;
+        var newFilePath = videosDestination + fileDate.year + "/" + fileDate.monthNumber + " - " + fileDate.monthName + "/" + file;
+
+        console.log(newFilePath);
+
+        // Move the file
+        // mkdirp: Make the required directories if they don't exist
+        // clobber: Don't overwrite any files
+        mv(existingFilePath, newFilePath, {mkdirp: true, clobber: false}, function(err) {
+            if(err) {
+                throw err;
+            }
+
+            console.log("Moving " + file + " to " + newFilePath);
+        })
+    }
+})
+
+// Function to get the date information
+function getDateInfo(file) {
+    var year = file.substring(0,4);
+    var monthNumber = file.substring(5,7);
+    var monthName = months[monthNumber - 1];
+
+    console.log(monthNumber);
+
+    // Add a leading 0 to single digit months
+    if (monthNumber.length === 1) {
+        monthNumber = "0" + month;
+    }
+
+    return {
+        year: year,
+        monthNumber: monthNumber,
+        monthName: monthName
+    };
+}
